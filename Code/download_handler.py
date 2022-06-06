2520ab57398bd4d0d5384bf6e7d925380fe2efa1
Johannes-Zi <19johannes99@web.de>
2022-06-06 17:00:31 +0200

    temp

--- Code/download_handler.py
+++ Code/download_handler.py
@@ -1,7 +1,12 @@
 import tweepy
 import configparser
 import pandas
+import os
 from datetime import datetime
+#import logging
+
+# Basic configuration
+#logging.basicConfig(level=logging.DEBUG)
 
 
 class DownloadHandler:
@@ -47,12 +52,46 @@ class DownloadHandler:
         # Create client
         client = tweepy.Client(bearer_token=self.bearer_token)
 
-        response = client.search_recent_tweets(query=query, tweet_fields=["created_at"])
+        # Die folgenden Daten sollten wir für jeden tweet speichern, den unteschied zwischen tweet geo und place geo hab
+        # ich noch nicht ganz verstanden
+        """response = client.search_recent_tweets(query=query, max_results=10,
+                                               tweet_fields=["id", "created_at", "author_id", "text", "entities", "geo",
+                                                             "in_reply_to_user_id", "lang", "possibly_sensitive",
+                                                             "public_metrics", "source"],
+                                               user_fields=["id", "created_at", "location", "public_metrics"],
+                                               place_fields=["id", "contained_within", "geo", "name", "place_type"])"""
+
+
+    # Ich glaube mit Paginator kann man auf die user_fileds us zugreifen
+        """for tweet in tweepy.Paginator(client.search_recent_tweets, "Tweepy",
+                                      max_results=100).flatten(limit=250):
+            print(tweet.id)"""
+
+        response = client.search_recent_tweets(query=query, max_results=10,
+                                               expansions=["author_id"],
+                                               tweet_fields=["source"],
+                                               user_fields=["public_metrics"],
+                                               place_fields=["place_type"])
         # Format the data
         tweet_data = []
+
+        # Wie hier auf tweet field zugreifen user.data geht ja nicht, verstehe expansions und includes nicht ganz
+
+        temp = dir(response.includes)
+
+        print(temp)
+        exit()
+
         for tweet in response.data:
-            text = tweet.text.strip()
-            tweet_data.append([tweet.created_at, text])
+            # Extract relevant data
+            """tweet_data.append([tweet.id, tweet.created_at, tweet.author_id, tweet.text.strip(), tweet.entities,
+                               tweet.geo, tweet.in_reply_to_user_id, tweet.lang, tweet.possibly_sensitive,
+                               tweet.public_metrics, tweet.source])"""
+
+            """attributes = dir(tweet)
+            print(attributes)
+            exit()"""
+            tweet_data.append([tweet.public_metrics])
 
         return tweet_data
 
@@ -67,14 +106,18 @@ class DownloadHandler:
 
 
 def main():
+
     download_handler = DownloadHandler()
-    download_handler.read_config_file("Data/config.ini")
+    key_location = "Data/config.ini"
+    download_handler.read_config_file(key_location)
     download_handler.create_api_interface()
 
-    query = "@DB OR Deutsche Bahn OR Die Bahn -is:retweet"
+    query = "@DB OR Deutsche Bahn OR Die Bahn -is:retweet"      # excludes retweets
     tweets = download_handler.get_recent_tweets(query)
+    for x in tweets:
+        print(x)
     columns = ["Time", "Tweet"]
-    download_handler.save_tweets(tweets, columns)
+    #download_handler.save_tweets(tweets, columns)
 
 
 if __name__ == '__main__':
