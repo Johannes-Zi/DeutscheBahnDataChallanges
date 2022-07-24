693d1d1ab2efbf0dabc906c1cbdc9ca3ece48904
Christian <christian.kolland@web.de>
2022-07-24 14:58:47 +0200

    Added plotting method for Geo Tweets

--- Code/tweet_mapper.py
+++ Code/tweet_mapper.py
@@ -10,21 +10,35 @@ import geopandas as gpd
 
 class TweetMapper:
 
-    def __init__(self, json_file):
-        self.json_file = json_file
-        self.locations_file = None
-        self.tweet_data = self.get_data(self.json_file)
+    def __init__(self, twitter_json_file=None, geo_tweets_json_file=None):
+        self.twitter_json_file = twitter_json_file
+        self.geo_tweets_json_file = geo_tweets_json_file
+        self.tweet_data = {}
         self.geo_tweets = []
         self.locations = {}
 
+    def get_locations(self, location_json_file):
+        with open(location_json_file, 'r', encoding='utf-8') as in_file:
+            self.locations = json.load(in_file)
+
     @staticmethod
-    def get_data(json_file):
-        with open(json_file, 'r') as in_file:
+    def get_tweets(json_file):
+        """
+        Gets a dataset filled with Tweets and saves them in a dictionary.
+
+        :param json_file: JSON file containing Twitter data
+        """
+
+        with open(json_file, 'r', encoding='utf-8') as in_file:
             data = json.load(in_file)
 
         return data
 
-    def extract_geo_data(self):
+    def extract_geo(self):
+        """
+        Extracts all Tweets from the dataset that have a Geo object attached.
+        """
+
         self.geo_tweets = {}
         for tweet_id in self.tweet_data:
             tweet = self.tweet_data[tweet_id]
@@ -32,7 +46,13 @@ class TweetMapper:
             if all(tweet['Geo'].values()):
                 self.geo_tweets[tweet_id] = tweet
 
-    def save_data(self, data_file=None):
+    def save_geo_tweets(self, data_file=None):
+        """
+        Saves the extracted Tweets into a JSON file that can be expanded.
+
+        :param data_file: If a data file is given, the values are attached to the file
+        """
+
         if data_file is not None:
             with open(data_file, 'a', encoding='utf-8') as out_file:
                 json.dump(self.geo_tweets, out_file, ensure_ascii=False, indent=4)
@@ -41,34 +61,35 @@ class TweetMapper:
             with open('Geo_Tweets' + time + '.json', 'w', encoding='utf-8') as out_file:
                 json.dump(self.geo_tweets, out_file, ensure_ascii=False, indent=4)
 
+    def extract_geo_tweets(self):
+        """
+        Handles the extraction process of geo Tweets.
+        """
+
+        self.tweet_data = self.get_tweets(self.twitter_json_file)
+        self.extract_geo()
+        self.save_geo_tweets()
+
+    def get_locations(self, json_file):
+        """
+        Gets the location da
+        :param json_file: JSON file that contains the information
+        """
+
+        with open(json_file, "r", encoding="utf-8") as in_file:
+            self.locations = json.load(in_file)
+
     def update_locations(self):
-        df = pd.read_json(self.json_file, orient='index', convert_axes=False)
-
         """
-        # Latitude between -90.00 and 90.00
-        # Longitude between -180.00 and 180.00
-        min_longitude = 90
-        min_latitude = 180
-        max_longitude = -90
-        max_latitude = -180
-        for i in range(len(df['Geo'])):
-            if df['Geo'][i]['Country_Code'] == 'DE':
-                if df['Geo'][i]['Geo']['bbox'][0] < min_longitude:
-                    min_longitude = df['Geo'][i]['Geo']['bbox'][0]
-                if df['Geo'][i]['Geo']['bbox'][1] < min_latitude:
-                    min_latitude = df['Geo'][i]['Geo']['bbox'][1]
-                if df['Geo'][i]['Geo']['bbox'][2] > max_longitude:
-                    max_longitude = df['Geo'][i]['Geo']['bbox'][2]
-                if df['Geo'][i]['Geo']['bbox'][3] > max_latitude:
-                    max_latitude = df['Geo'][i]['Geo']['bbox'][3]
-
-        bounding_box = (min_longitude, min_latitude, max_longitude, max_latitude)
+        Creates a location file with latitude and longitude for all locations found in the geo Tweets.
         """
 
-        for i in range(len(df['Geo'])):
-            if df['Geo'][i]['Country_Code'] == 'DE':
-                if df['Geo'][i]['Name'] not in self.locations:
-                    self.locations[df['Geo'][i]['Name']] = None
+        self.geo_tweets = pd.read_json(self.geo_tweets_json_file, orient='index', convert_axes=False)
+
+        for i in range(len(self.geo_tweets['Geo'])):
+            if self.geo_tweets['Geo'][i]['Country_Code'] == 'DE':
+                if self.geo_tweets['Geo'][i]['Name'] not in self.locations:
+                    self.locations[self.geo_tweets['Geo'][i]['Name']] = None
 
         for place in self.locations.keys():
             if self.locations[place] is None:
@@ -80,40 +101,83 @@ class TweetMapper:
                 else:
                     self.locations[place] = {"Latitude": location.latitude, "Longitude": location.longitude}
 
-    def get_locations(self, file_path):
-        self.locations_file = file_path
-        with open(file_path, "r", encoding="utf-8") as in_file:
-            self.locations = json.load(in_file)
+    def save_locations(self, locations_database=None):
+        """
+        Saves the locations in a JSON file.
+        """
 
-    def save_locations(self):
-        if self.locations_file is not None:
-            with open(self.locations_file, "a", encoding='utf-8') as out_file:
+        if locations_database is not None:
+            with open(locations_database, "a", encoding='utf-8') as out_file:
                 json.dump(self.locations, out_file, indent=4)
         else:
             time = datetime.now().strftime("%d-%m-%Y_%H-%M")
-            with open('location_database_' + time + '.json', 'w', encoding='utf-8') as out_file:
+            with open('locations_database_' + time + '.json', 'w', encoding='utf-8') as out_file:
                 json.dump(self.locations, out_file, ensure_ascii=False, indent=4)
 
+    def add_locations(self):
+        with open(self.geo_tweets_json_file, "r", encoding="utf-8") as in_file:
+            self.geo_tweets = json.load(in_file)
+
+            for entry in self.geo_tweets.keys():
+                location = self.geo_tweets[entry]['Geo']['Name']
+                if location in self.locations.keys():
+                    self.geo_tweets[entry]['Geo']['Place'] = self.locations[location]
+
+        with open(self.geo_tweets_json_file, 'w', encoding="utf-8") as out_file:
+            json.dump(self.geo_tweets, out_file, indent=4)
+
+    def extract_locations(self, locations_database):
+        """
+        Handles the location extraction process.
+
+        :param locations_database: Path to locations database file
+        """
+
+        if locations_database:
+            self.get_locations(locations_database)
+            self.update_locations()
+            self.save_locations(locations_database)
+        else:
+            self.update_locations()
+            self.save_locations()
+
     @staticmethod
-    def plot_data(self, json_file):
-        df = pd.read_json(json_file, orient='index')
+    def plot_geo_data(json_file):
 
-        to_drop = []
-        for i in range(len(df.index)):
-            if df['Latitude'][i] == "n/a" or df['Longitude'][i] == "n/a":
-                row = df.iloc[[i]]
-                to_drop.append(row.index[0])
+        with open(json_file, 'r', encoding='utf-8') as in_file:
+            data = json.load(in_file)
 
-        df = df.drop(to_drop)
+            df = {}
+            for entry in data.keys():
+                values = data[entry]
+                if values['Geo']['Country_Code'] == 'DE':
+                    df[values['Data']['Id']] = {'User': values['User']['Id'],
+                                                'Latitude': values['Geo']['Place']['Latitude'],
+                                                'Longitude': values['Geo']['Place']['Longitude'],
+                                                'Sentiment': values['Data']['Sentiment'],
+                                                'Created_At': values['Data']['Created_At']}
 
-        pd.set_option('display.max_rows', None)
-        print(df)
+            df = pd.DataFrame.from_dict(df, orient='Index')
 
-        fig, ax = plt.subplots(figsize=(8, 6))
-        countries = gpd.read_file(gpd.datasets.get_path("naturalearth_lowres"))
-        countries[countries["name"] == "Germany"].plot(color="lightgrey", ax=ax)
-        df.plot(x="Longitude", y="Latitude", kind="scatter", colormap="YlOrRd", title=f"Test", ax=ax)
-        plt.show()
+            to_drop = []
+            for index, row in df.iterrows():
+                if row['Latitude'] == "n/a" or row['Longitude'] == "n/a":
+                    to_drop.append(index)
+            df = df.drop(to_drop)
+
+            pandas.set_option('display.max_rows', None)
+            print(df)
+
+            cm = 1 / 2.54  # centimeters in inches
+            fig, ax = plt.subplots(figsize=(20*cm, 28*cm))
+            countries = gpd.read_file(gpd.datasets.get_path("naturalearth_lowres"))
+            countries[countries["name"] == "Germany"].plot(color="lightgrey", ax=ax)
+            df.plot(x="Longitude", y="Latitude", kind="scatter", c="Sentiment", colormap="YlOrRd",
+                    title=f"Distribution Germany", ax=ax)
+            plt.savefig("Distribution Germany.pdf")
+
+    def plot_new_data(self, csv):
+        pass
 
 
 def main():
@@ -121,12 +185,12 @@ def main():
     geo_file = "/home/ubuntu/Projects/DeutscheBahnDataChallanges/Data/Geo_Tweets21-07-2022_17-30.json"
     locations = "/home/ubuntu/Projects/DeutscheBahnDataChallanges/Data/location_database_24-07-2022_11-38.json"
 
-    new_mapper = TweetMapper(geo_file)
-    # new_mapper.extract_geo_data()
-    # new_mapper.save_data()
-    # new_mapper.update_locations()
-    # new_mapper.save_locations()
-    # new_mapper.plot_data(locations)
+    new_mapper = TweetMapper(json_file, geo_file)
+    # new_mapper.extract_geo_tweets()
+    # new_mapper.extract_locations(locations)
+    new_mapper.plot_geo_data(geo_file)
+    # new_mapper.get_locations(locations)
+    # new_mapper.add_locations()
 
 
 if __name__ == '__main__':
